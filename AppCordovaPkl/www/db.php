a0c7c450236c31521bb830d0208e897214c40888
FranitaTriandhani <franitatriandhani24@gmail.com>
2019-04-26 01:49:47 +0700

    first commit

---
+++ AppCordovaPkl/www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id8146776_pkl2franita","pklfranita","id8146776_pkl2") or die ("could not connect database");
+?>
